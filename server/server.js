1440ecf4b4a07bd7fffa75fa9f09ce401be5b74e
Prateek Chawla <prateek.chawla98@gmail.com>
2020-09-14 16:06:21 +0530

    Fixed Path

--- server/server.js
+++ server/server.js
@@ -88,10 +88,10 @@ io.on("connection", socket => {
 	});
 });
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "/../client/build")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "../client/build/index.html"));
+	res.sendFile(path.join(__dirname + "/../client/build/index.html"));
 });
 
 server.listen(PORT, () => console.log(`Socket IO PORT# ${PORT}`));
