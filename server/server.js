7efcbc8c33e828272f9e7a26c31de2df72d8ed98
Prateek Chawla <prateek.chawla98@gmail.com>
2020-09-14 15:42:18 +0530

    Fixed express import

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-const app = require("express")();
+const express = require("express");
+const path = require("path");
+const app = express();
 const server = require("http").createServer(app);
 const socketIo = require("socket.io");
 const QuizManager = require("./Entities/quizManager");
